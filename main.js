d665a164b09daa91800407dcf33fc9ec61369227
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-31 16:07:55 +0200

    generated post by javascript

--- main.js
+++ main.js
@@ -55,28 +55,28 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
-const divElementContainer = document.querySelector('div.container');
+const divElementContainer = document.getElementById('container');
 
-posts.forEach(( imageElement, index ) => {
+posts.forEach(( postElement, index ) => {
     divElementContainer.innerHTML +=
 
     ` <div class="post">
     <div class="post__header">
         <div class="post-meta">                    
             <div class="post-meta__icon">
-                <img class="profile-pic" src="${imageElement.image}" alt="Phil Mangione">                    
+                <img class="profile-pic" src="${postElement.author.image}" alt="Phil Mangione">                    
             </div>
             <div class="post-meta__data">
                 <div class="post-meta__author">
-                  "${imageElement.name}" 
+                  "${postElement.author.name}" 
                 </div>
                 <div class="post-meta__time">4 mesi fa</div>
             </div>                    
         </div>
     </div>
-    <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+    <div class="post__text">${postElement.content}</div>
     <div class="post__image">
-        <img src="https://unsplash.it/600/300?image=171" alt="">
+        <img src="${postElement.media}" alt="">
     </div>
     <div class="post__footer">
         <div class="likes js-likes">
@@ -87,8 +87,9 @@ posts.forEach(( imageElement, index ) => {
                 </a>
             </div>
             <div class="likes__counter">
-                Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                Piace a <b id="like-counter-1" class="js-likes-counter">${postElement.likes}</b> persone
             </div>
         </div> 
     </div>            
 </div>`
+});
