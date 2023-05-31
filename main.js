c88d99e24da1cc7cec4bbe310f8be9395ca5d54b
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-31 15:54:21 +0200

    doing the first post

--- main.js
+++ main.js
@@ -55,3 +55,40 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+const divElementContainer = document.querySelector('div.container');
+
+posts.forEach(( imageElement, index ) => {
+    divElementContainer.innerHTML +=
+
+    ` <div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${imageElement.image}" alt="Phil Mangione">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">
+                  "${imageElement.name}" 
+                </div>
+                <div class="post-meta__time">4 mesi fa</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+    <div class="post__image">
+        <img src="https://unsplash.it/600/300?image=171" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+            </div>
+        </div> 
+    </div>            
+</div>`
